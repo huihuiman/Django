e67ead2683f4d9c42f6bee595203203eb3a215fe
Chih-Hui <53479045+huihuiman@users.noreply.github.com>
2020-03-09 16:33:45 +0800

    Update views.py

--- index/views.py
+++ index/views.py
@@ -8,19 +8,12 @@ from .forms import *
 
 def login_views(request):
   if request.method == 'GET':
-    #　獲取來訪地址，無則導回首頁
     url = request.META.get('HTTP_REFERER','/')
-    #get請求　－　判斷session,判斷cookie
-    #先判斷session中是否有登錄訊息
     if 'uid' in request.session and 'uphone' in request.session:
-      #有登錄訊息保存在　session
       resp = HttpResponseRedirect(url)
       return resp
     else:
-      #沒有登錄訊息保存在　session，繼續判斷cookies中是否有登錄訊息
       if 'uid' in request.COOKIES and 'uphone' in request.COOKIES:
-        #cookies中有登錄訊息　
-        #将cookies中的內容取出来保存進session
         uid = request.COOKIES['uid']
         uphone = request.COOKIES['uphone']
         request.session['uid']=uid
@@ -28,36 +21,27 @@ def login_views(request):
         resp = redirect(url)
         return resp
       else:
-        #cookies中没有登錄訊息
         form = LoginForm()
-        #將來訪地址保存進cookies中
         resp = render(request,'login.html',locals())
         resp.set_cookie('url',url)
         return resp
   else:
-    #post請求
-    #獲取帳號密碼
     uphone = request.POST['uphone']
     upwd = request.POST['upwd']
-    #判斷號與密碼是否存在
     users=User.objects.filter(uphone=uphone,upwd=upwd)
     if users:
-      #登錄成功：先存進session
       request.session['uid']=users[0].id
       request.session['uphone']=uphone
       url = request.COOKIES.get('url','/')
       resp = redirect(url)
-      #將url从cookies中删除出去
       if 'url' in request.COOKIES:
         resp.delete_cookie('url')
-      #判斷是否要存進cookies
       if 'isSaved' in request.POST:
         expire = 60*60*24*30
         resp.set_cookie('uid',users[0].id,expire)
         resp.set_cookie('uphone',uphone,expire)
       return resp
     else:
-      #登錄失敗
       form = LoginForm()
       return render(request,'login.html',locals())
 
@@ -65,9 +49,7 @@ def register_views(request):
   if request.method == 'GET':
     return render(request,'register.html')
   else:
-    # 驗證帳號是否已存在
     uphone = request.POST['uphone']
-    #接收數據存進數據庫
     upwd = request.POST['upwd']
     uname = request.POST['uname']
     uemail = request.POST['uemail']
@@ -77,7 +59,6 @@ def register_views(request):
     user.uname = uname
     user.uemail = uemail
     user.save()
-    #取出user中的id 和 uphone的值保存進session
     request.session['uid']=user.id
     request.session['uphone']=user.uphone
     return redirect('/')
@@ -151,7 +132,6 @@ def check_login_views(request):
     }
     return HttpResponse(json.dumps(dic))
 
-#一般退出
 def logout_views(request):
   if 'uid' in request.session and 'uphone' in request.session:
     del request.session['uid']
@@ -164,7 +144,6 @@ def logout_views(request):
     return resp
   return redirect('/')
 
-# 購物車頁面退出回首頁
 def logout_cart_views(request):
   if 'uid' in request.session and 'uphone' in request.session:
     del request.session['uid']
@@ -176,7 +155,6 @@ def logout_cart_views(request):
     return resp
   return redirect('/')
 
-#加載所有的商品類型以及對應的每個類型底下的前10條數據
 def type_goods_views(request):
   all_list = []
   types = GoodsType.objects.all()
@@ -208,7 +186,6 @@ def add_cart_views(request):
       'statusText':'更新数量成功'
     }
   else:
-    #沒有此商品,做新增商品的動作
     cartinfo = CartInfo()
     cartinfo.user_id = user_id
     cartinfo.goods_id = good_id
